99eefb96a71b6f96c4dcb88a68b5a0ce6fad3540
ErathorS <tacio.barbosa84@gmail.com>
2025-04-13 20:19:03 -0300

    Revert "Sistemas de Coleta de itens em Terceira Pessoa"
    This reverts commit 34e1e3b46e56593a3c824573f1f4b1f112fc3261.

--- Assets/Scripts/Pegar_e_Soltar_Itens.cs
+++ Assets/Scripts/Pegar_e_Soltar_Itens.cs
@@ -6,14 +6,10 @@ public class Pegar_e_Soltar_Itens : MonoBehaviour
     [SerializeField] Transform ObjtGrabPointTransform;
     [SerializeField] LayerMask pickupLayerMask;
     ObjetoSeguravel objetoSeguravel;
-    public float pickupDistance = 10f;
+    float pickupDistance = 2f;
 
     void Update()
     {
-        // if (Physics.Raycast(PlayerCameraTransform.position, PlayerCameraTransform.forward, out RaycastHit raycastHit, pickupDistance, pickupLayerMask))
-        // {
-        //         Debug.Log(raycastHit.transform.name);
-        // }
         if (Input.GetKeyDown(KeyCode.E)) // Verifica se a tecla "E" foi pressionada
         {
             if(objetoSeguravel == null) // Se já estiver segurando um objeto, solta-o
