4064bcbabc58153c38a09b847aa36e3b9152b42e
DvD071 <davidsuzartaraujo@gmail.com>
2025-05-27 21:08:38 -0300

    Toque

---
+++ Assets/Scripts/BotaoInterativo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotaoInterativo : MonoBehaviour
+{
+   public void ExecutarAcao()
+    {
+        Debug.Log("Ação do objeto interativo executada: " + name);
+        // Aqui você pode fazer algo: abrir uma porta, tocar um som, etc.
+    }
+    
+}
