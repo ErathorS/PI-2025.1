12e94e1b008ccdf1f638b543dd46f31c57931277
SenshiZ <diegocamposmelo234@gmail.com>
2025-06-19 20:43:02 -0300

    QuickFix (Colect Sis)

--- Assets/Scripts/Colect_Sis.cs
+++ Assets/Scripts/Colect_Sis.cs
@@ -9,7 +9,12 @@ public class Colect_Sis : MonoBehaviour
     public static int Cur_Collect = 0;
 
     public TextMeshProUGUI TextCount;
+    public GameObject FileButton;
     // Update is called once per frame
+    private void Start()
+    {
+        Cur_Collect = 0;
+    }
     void Update()
     {
         TextCount.text = Cur_Collect + "/" + To_Collect + " Coletaveis";
@@ -21,7 +26,16 @@ public class Colect_Sis : MonoBehaviour
         {
             Destroy(other.gameObject);
             Cur_Collect++;
+            if(To_Collect == Cur_Collect) 
+            {
+                FileButton.SetActive(true);
+            }
         }
         
+    }
+
+    public void ArquivoPlay() 
+    {
+        print("Arquivo Wow");
     }
 }
