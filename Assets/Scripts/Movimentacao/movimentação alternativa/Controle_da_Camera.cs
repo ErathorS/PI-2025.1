1decb48c63f4690c2de5ea3a8fa28da02e1068c7
ErathorS <tacio.barbosa84@gmail.com>
2025-06-26 10:10:12 -0300

    Correções finais

--- Assets/Scripts/Movimentacao/movimentação alternativa/Controle_da_Camera.cs
+++ Assets/Scripts/Movimentacao/movimentação alternativa/Controle_da_Camera.cs
@@ -6,6 +6,7 @@ public class Controle_da_Camera : MonoBehaviour
     private Transform character;
     public float sensitivity = 2f;
     public float smoothing = 1.5f;
+    private float initialYRotation;
 
     private Vector2 velocity;
     private Vector2 frameVelocity;
@@ -18,20 +19,30 @@ public class Controle_da_Camera : MonoBehaviour
 
     void Start()
     {
-        //Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        initialYRotation = character.localEulerAngles.y;
     }
 
     void Update()
     {
-        Vector2 mouseDelta = new Vector2(joystickRotation.Horizontal, joystickRotation.Vertical);
-        Vector2 rawFrameVelocity = Vector2.Scale(mouseDelta, Vector2.one * sensitivity);
-        frameVelocity = Vector2.Lerp(frameVelocity, rawFrameVelocity, 1f / smoothing);
-        velocity += frameVelocity;
+        Vector2 input = new Vector2(joystickRotation.Horizontal, joystickRotation.Vertical);
 
-        // Impede olhar para baixo (limita a rotação a no máximo olhar reto)
-        velocity.y = Mathf.Clamp(velocity.y, -25f, 2f);
-        transform.localRotation = Quaternion.AngleAxis(-velocity.y, Vector3.right);
-        character.localRotation = Quaternion.AngleAxis(velocity.x, Vector3.up);
+        if (input.magnitude > 0.1f)
+        {
+            Vector2 rawFrameVelocity = input * sensitivity;
+            frameVelocity = Vector2.Lerp(frameVelocity, rawFrameVelocity, 1f / smoothing);
+            velocity += frameVelocity;
+
+            velocity.y = Mathf.Clamp(velocity.y, -25f, 2f);
+            transform.localRotation = Quaternion.AngleAxis(-velocity.y, Vector3.right);
+            character.localRotation = Quaternion.Euler(0, initialYRotation + velocity.x, 0);
+        }
+    }
+
+    public void ResetCamera()
+    {
+        velocity = Vector2.zero;
+        character.localRotation = Quaternion.Euler(0, initialYRotation, 0);
+        transform.localRotation = Quaternion.identity;
     }
 }
