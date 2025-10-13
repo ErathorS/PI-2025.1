d655be2f2477c42df3ee37aef6f747c226ac5910
ErathorS <tacio.barbosa84@gmail.com>
2025-10-13 19:20:34 -0300

    Players Animations e prefabs

--- Assets/ProjetoOd/Scripts/Dialogo/PlayerIdentifier.cs
+++ Assets/ProjetoOd/Scripts/Dialogo/PlayerIdentifier.cs
@@ -8,6 +8,6 @@ public class PlayerIdentifier : MonoBehaviourPun
     void Start()
     {
         actorID = photonView.Owner.ActorNumber;
-        gameObject.name = $"PI_MC_{actorID}";
+        gameObject.name = $"Player_{actorID}";
     }
 }
