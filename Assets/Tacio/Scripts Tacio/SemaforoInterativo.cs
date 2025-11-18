03bab910f45b8e98d09cfa7eebdcc2809bd45f01
ErathorS <tacio.barbosa84@gmail.com>
2025-11-17 23:14:01 -0300

    semaforo

--- Assets/Tacio/Scripts Tacio/SemaforoInterativo.cs
+++ Assets/Tacio/Scripts Tacio/SemaforoInterativo.cs
@@ -1,53 +1,98 @@
 using UnityEngine;
 using Photon.Pun;
-using UnityEngine.UI;
 
-[RequireComponent(typeof(Collider))]
 public class SemaforoInterativo : MonoBehaviourPun
 {
-    public SemaforoController semaforo;
+    [Header("Configuração")]
+    public Light luzSemaforo;
+    public Color corVerde = Color.green;
+    public Color corVermelho = Color.red;
+    public float tempoVermelho = 5f;
 
-    void Start()
+    private bool semaforoAtivo = false;
+    private float timer = 0f;
+
+    private void Start()
     {
-        Collider col = GetComponent<Collider>();
-        col.isTrigger = true;
+        if (luzSemaforo != null)
+            luzSemaforo.color = corVerde;
     }
 
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
 
         PhotonView pv = other.GetComponent<PhotonView>();
-        if (pv == null || !pv.IsMine) return;
-
-        PlayerUIReferences ui = other.GetComponentInChildren<PlayerUIReferences>();
-        if (ui == null || ui.botaoInteragir == null) return;
-
-        ui.botaoInteragir.gameObject.SetActive(true);
-        ui.botaoInteragir.onClick.RemoveAllListeners();
-        ui.botaoInteragir.onClick.AddListener(() => OnPlayerInteract(ui));
+        if (pv != null && pv.IsMine)
+        {
+            var ui = other.GetComponentInChildren<PlayerUIReferences>();
+            if (ui != null && ui.botaoInteracao != null)   // ← NOME REAL DO SEU CAMPO
+            {
+                ui.botaoInteracao.gameObject.SetActive(true);
+                ui.botaoInteracao.onClick.RemoveAllListeners();
+                ui.botaoInteracao.onClick.AddListener(() => AtivarSemaforo(pv.OwnerActorNr));
+            }
+        }
     }
 
-    void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider other)
     {
         if (!other.CompareTag("Player")) return;
 
         PhotonView pv = other.GetComponent<PhotonView>();
-        if (pv == null || !pv.IsMine) return;
-
-        PlayerUIReferences ui = other.GetComponentInChildren<PlayerUIReferences>();
-        if (ui == null || ui.botaoInteragir == null) return;
-
-        ui.botaoInteragir.onClick.RemoveAllListeners();
-        ui.botaoInteragir.gameObject.SetActive(false);
+        if (pv != null && pv.IsMine)
+        {
+            var ui = other.GetComponentInChildren<PlayerUIReferences>();
+            if (ui != null && ui.botaoInteracao != null)
+            {
+                ui.botaoInteracao.gameObject.SetActive(false);
+                ui.botaoInteracao.onClick.RemoveAllListeners();
+            }
+        }
     }
 
-    void OnPlayerInteract(PlayerUIReferences ui)
+    private void AtivarSemaforo(int actorID)
     {
-        if (semaforo != null)
-            semaforo.PedirAcionarSemaforo();
+        if (!photonView.IsMine)
+        {
+            photonView.RPC("RPC_AtivarSemaforo", RpcTarget.MasterClient, actorID);
+            return;
+        }
 
-        ui.botaoInteragir.onClick.RemoveAllListeners();
-        ui.botaoInteragir.gameObject.SetActive(false);
+        RPC_AtivarSemaforo(actorID);
+    }
+
+    [PunRPC]
+    private void RPC_AtivarSemaforo(int actorID)
+    {
+        if (semaforoAtivo) return;
+
+        semaforoAtivo = true;
+        timer = tempoVermelho;
+
+        if (luzSemaforo != null)
+            luzSemaforo.color = corVermelho;
+    }
+
+    private void Update()
+    {
+        if (!semaforoAtivo) return;
+
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            if (photonView.IsMine)
+                photonView.RPC("RPC_DesativarSemaforo", RpcTarget.AllBuffered);
+        }
+    }
+
+    [PunRPC]
+    private void RPC_DesativarSemaforo()
+    {
+        semaforoAtivo = false;
+
+        if (luzSemaforo != null)
+            luzSemaforo.color = corVerde;
     }
 }
