b6c3cc64f1bda89c0a7cdcbb3609b42922408415
ErathorS <tacio.barbosa84@gmail.com>
2025-11-26 15:59:38 -0300

    transito, dialogos npcs

--- Assets/ProjetoOd/Scripts/CarMovemetn.cs
+++ Assets/ProjetoOd/Scripts/CarMovemetn.cs
@@ -5,17 +5,21 @@ public class CarMovemetn : MonoBehaviour
     public float DeSlow = 0;
     public Vector3 SpawnPlayer = new Vector3(-2.5f, -0.001f, -7.4f);
 
+    public bool DirecaoContraria = false;  // 👈 NOVO
+
     void Update()
     {
-        transform.Translate(transform.right * Time.deltaTime * DeSlow);
+        // 👇 Escolhe a direção (1 normal, -1 invertida)
+        float direction = DirecaoContraria ? -1f : 1f;
+
+        transform.Translate(transform.right * Time.deltaTime * DeSlow * direction);
 
         RaycastHit hit;
 
-        if (Physics.Raycast(transform.position, transform.right, out hit, 15))
+        if (Physics.Raycast(transform.position, transform.right * direction, out hit, 15))
         {
             if (hit.transform.CompareTag("Stop Car"))
             {
-                // 🚦 Verifica o estado do semáforo
                 if (BotaoInterativo.StateSemaforo)
                 {
                     DeSlow = 0;
@@ -28,15 +32,22 @@ public class CarMovemetn : MonoBehaviour
 
             if (hit.transform.CompareTag("Respawn"))
             {
-                transform.position = new Vector3(transform.position.x - Random.Range(120, 150), transform.position.y, transform.position.z);
+                transform.position = new Vector3(
+                    transform.position.x - Random.Range(120, 150) * direction,
+                    transform.position.y,
+                    transform.position.z
+                );
             }
 
-            if (hit.transform.CompareTag("Player") && DeSlow > 5 && Vector3.Distance(transform.position, hit.transform.position) < 5)
+            if (hit.transform.CompareTag("Player") && DeSlow > 5 &&
+                Vector3.Distance(transform.position, hit.transform.position) < 5)
             {
                 hit.collider.gameObject.transform.position = SpawnPlayer;
             }
 
-            if (DeSlow < 15 && !hit.transform.CompareTag("Player") && !hit.transform.CompareTag("Respawn") && !hit.transform.CompareTag("Stop Car"))
+            if (DeSlow < 15 && !hit.transform.CompareTag("Player") &&
+                !hit.transform.CompareTag("Respawn") &&
+                !hit.transform.CompareTag("Stop Car"))
             {
                 DeSlow += 0.1f;
             }
